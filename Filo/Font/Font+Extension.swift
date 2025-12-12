09e86bc1a4de16ec65e8b3319e70629846316085
rmfls0606 <rmfls0606@naver.com>
2025-12-13 00:22:39 +0900

    feat: 폰트 추가 및 정의

---
+++ Filo/Font/Font+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  Font+Extension.swift
+//  Filo
+//
+//  Created by 이상민 on 12/12/25.
+//
+
+import UIKit
+
+extension UIFont{
+    static let title1 = UIFont(name: "Pretendard-Bold", size: 20)
+    
+    static let body1 = UIFont(name: "Pretendard-Medium", size: 16)
+    static let body2 = UIFont(name: "Pretendard-Medium", size: 14)
+    static let body3 = UIFont(name: "Pretendard-Medium", size: 13)
+    
+    static let caption1 = UIFont(name: "Pretendard-Regular", size: 12)
+    static let caption2 = UIFont(name: "Pretendard-Regular", size: 10)
+    static let caption3 = UIFont(name: "Pretendard-Regular", size: 8)
+}
