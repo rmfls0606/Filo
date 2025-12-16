2323ffda79afbe44ee73ab059d8aa1e8cc698f4a
rmfls0606 <rmfls0606@naver.com>
2025-12-17 01:30:35 +0900

    feat: 화면 단위 폴더 구조 정의 및 파일 생성

---
+++ Filo/Feature/Feed/FeedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FeedViewController.swift
+//  Filo
+//
+//  Created by 이상민 on 12/17/25.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
