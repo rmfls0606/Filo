defbd4e2f283d2283d7c540c8f858fe1a89f7e79
rmfls0606 <rmfls0606@naver.com>
2025-12-17 13:29:19 +0900

    feat: 학교안심 물결체 폰트 추가

--- Filo/Core/DesignSystem/Font/Font+Extension.swift
+++ Filo/Core/DesignSystem/Font/Font+Extension.swift
@@ -8,13 +8,23 @@
 import UIKit
 
 extension UIFont{
-    static let title1 = UIFont(name: "Pretendard-Bold", size: 20)
+    enum Pretendard{
+        static let title1 = UIFont(name: "Pretendard-Bold", size: 20)
+        
+        static let body1 = UIFont(name: "Pretendard-Medium", size: 16)
+        static let body2 = UIFont(name: "Pretendard-Medium", size: 14)
+        static let body3 = UIFont(name: "Pretendard-Medium", size: 13)
+        
+        static let caption1 = UIFont(name: "Pretendard-Regular", size: 12)
+        static let caption2 = UIFont(name: "Pretendard-Regular", size: 10)
+        static let caption3 = UIFont(name: "Pretendard-Regular", size: 8)
+    }
     
-    static let body1 = UIFont(name: "Pretendard-Medium", size: 16)
-    static let body2 = UIFont(name: "Pretendard-Medium", size: 14)
-    static let body3 = UIFont(name: "Pretendard-Medium", size: 13)
-    
-    static let caption1 = UIFont(name: "Pretendard-Regular", size: 12)
-    static let caption2 = UIFont(name: "Pretendard-Regular", size: 10)
-    static let caption3 = UIFont(name: "Pretendard-Regular", size: 8)
+    enum Mulggeol{
+        static let title1 = UIFont(name: "OTHakgyoansimMulgyeolB", size: 32)
+        
+        static let body1 = UIFont(name: "OTHakgyoansimMulgyeolB", size: 20)
+        
+        static let caption1 = UIFont(name: "OTHakgyoansimMulgyeolR", size: 14)
+    }
 }
