8b99535b74d0eae3681edc7c54b4e2a707a86379
rmfls0606 <rmfls0606@naver.com>
2026-01-11 17:16:05 +0900

    feat: 필터 속성 선택에 따른 상태 스트림 구현

--- Filo/Feature/Filter/FilterEditViewModel.swift
+++ Filo/Feature/Filter/FilterEditViewModel.swift
@@ -10,27 +10,46 @@ import RxSwift
 import RxCocoa
 
 final class FilterEditViewModel: ViewModelType {
-    struct Input { }
+    struct Input {
+        let selectedProp: ControlEvent<FilterPropItem>
+    }
     
     struct Output {
         let imageData: Driver<Data>
-        let filterProps: Driver<[FilterProps]>
+        let filterProps: Driver<[FilterPropItem]>
+        let selectedProp: Driver<FilterProps>
     }
     
     private let imageRelay: BehaviorRelay<Data>
+    private let disposeBag = DisposeBag()
     
     init(imageData: Data) {
         self.imageRelay = BehaviorRelay(value: imageData)
     }
     
     func transform(input: Input) -> Output {
-        let filterPropsRelay = BehaviorRelay<[FilterProps]>(
-            value: FilterProps.allCases
-        )
+        let filterPropsRelay = BehaviorRelay<[FilterPropItem]>(value: [])
+        let selectedPropRelay = BehaviorRelay<FilterProps>(value: .brightness)
+        
+        input.selectedProp
+            .map{ $0.prop }
+            .bind(to: selectedPropRelay)
+            .disposed(by: disposeBag)
+        
+        selectedPropRelay
+            .map { selected in
+                FilterProps.allCases.map { prop in
+                    FilterPropItem(prop: prop, isSelected: prop == selected)
+                }
+            }
+            .bind(to: filterPropsRelay)
+            .disposed(by: disposeBag)
+        
         
         return Output(
             imageData: imageRelay.asDriver(),
-            filterProps: filterPropsRelay.asDriver()
+            filterProps: filterPropsRelay.asDriver(),
+            selectedProp: selectedPropRelay.asDriver()
         )
     }
 }
