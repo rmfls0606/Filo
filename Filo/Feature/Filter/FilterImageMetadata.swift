3779baadd0fc890472d3e18b2d6bdcdd6733d55e
rmfls0606 <rmfls0606@naver.com>
2026-01-18 20:34:52 +0900

    feat: 메타데이터 모델 생성

---
+++ Filo/Feature/Filter/FilterImageMetadata.swift
@@ -0,0 +1,21 @@
+//
+//  FilterImageMetadata.swift
+//  Filo
+//
+//  Created by 이상민 on 01/xx/26.
+//
+
+import Foundation
+
+struct FilterImageMetadata {
+    let make: String?
+    let model: String?
+    let lensModel: String?
+    let focalLength: Double?
+    let fNumber: Double?
+    let iso: Int?
+    let megaPixel: Double?
+    let width: Int?
+    let height: Int?
+    let fileSizeMB: String?
+}
