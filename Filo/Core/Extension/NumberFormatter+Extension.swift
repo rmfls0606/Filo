b75a8858305378c762307b1487381f65f11ef26c
rmfls0606 <rmfls0606@naver.com>
2026-01-19 19:06:18 +0900

    feat: 숫자 3자리 (,)콤마 표시를 위한 포멧터 extension 추가

---
+++ Filo/Core/Extension/NumberFormatter+Extension.swift
@@ -0,0 +1,29 @@
+//
+//  NumberFormatter+Extension.swift
+//  Filo
+//
+//  Created by 이상민 on 01/19/26.
+//
+
+import Foundation
+
+extension NumberFormatter {
+    static let decimalFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 0
+        return formatter
+    }()
+}
+
+extension String {
+    var digitsOnly: String {
+        filter { $0.isNumber }
+    }
+
+    func formattedDecimal() -> String {
+        let digits = digitsOnly
+        guard let value = Int(digits) else { return "" }
+        return NumberFormatter.decimalFormatter.string(from: NSNumber(value: value)) ?? digits
+    }
+}
