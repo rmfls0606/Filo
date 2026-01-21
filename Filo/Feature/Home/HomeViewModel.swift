6a2ee366614fe20c3c67a006129197fc5b119924
rmfls0606 <rmfls0606@naver.com>
2026-01-22 01:41:50 +0900

    feat: 홈 화면 접근 시 오늘의 필터 API 호출 하도록 스트림 작성

--- Filo/Feature/Home/HomeViewModel.swift
+++ Filo/Feature/Home/HomeViewModel.swift
@@ -10,18 +10,37 @@ import RxSwift
 import RxCocoa
 
 final class HomeViewModel: ViewModelType{
+    //MARK: - Properties
+    private let disposeBag = DisposeBag()
+    
     struct Input{
         
     }
     
     struct Output{
         let filterCategories: Driver<[FilterCategoryType]>
+        let todayFilterData: Driver<TodayFilterResponseEntity>
     }
     
     func transform(input: Input) -> Output {
         let filterCategoriesRelay = BehaviorRelay<[FilterCategoryType]>(value: FilterCategoryType.allCases)
+        let todayFilterRelay = PublishRelay<TodayFilterResponseEntity>()
+        let networkErrorRelay = PublishRelay<NetworkError>()
+
+        Task{
+            do{
+                let dto: TodayFilterResponseDTO = try await NetworkManager.shared.request(FilterRouter.todayFilter)
+
+                todayFilterRelay.accept(dto.toEntity())
+            }catch(let error as NetworkError){
+                print(error)
+                networkErrorRelay.accept(error)
+            }
+        }
+        
         return Output(
-            filterCategories: filterCategoriesRelay.asDriver()
+            filterCategories: filterCategoriesRelay.asDriver(),
+            todayFilterData: todayFilterRelay.asDriver(onErrorDriveWith: .empty())
         )
     }
 }
