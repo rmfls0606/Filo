21e9434675d61bc365a71577b9f736f8b833daa5
rmfls0606 <rmfls0606@naver.com>
2026-01-23 01:50:03 +0900

    feat: 배너 화면에 보여줄 컬렉션 Cell UI 구현 및 반영 로직 작성

---
+++ Filo/Feature/Home/HomeBannerCollectionViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  HomeBannerCollectionViewCell.swift
+//  Filo
+//
+//  Created by 이상민 on 01/23/26.
+//
+
+import UIKit
+import SnapKit
+
+final class HomeBannerCollectionViewCell: BaseCollectionViewCell {
+    private let bannerImageView: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFill
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 12
+        return view
+    }()
+
+    override func configureHierarchy() {
+        contentView.addSubview(bannerImageView)
+    }
+
+    override func configureLayout() {
+        bannerImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+
+    func configure(urlString: String) {
+        bannerImageView.setKFImage(urlString: urlString)
+    }
+}
