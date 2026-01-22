7d3df310804e06d7e04b7fc3f088611c3e63a41c
rmfls0606 <rmfls0606@naver.com>
2026-01-22 17:29:16 +0900

    feat: 오늘의 작가 작품을 보여줄 컬렉션뷰 Cell UI 구성

---
+++ Filo/Feature/Home/TodayAuthorImageCollectionViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  TodayAuthorImageCollectionViewCell.swift
+//  Filo
+//
+//  Created by 이상민 on 1/22/26.
+//
+
+import UIKit
+import SnapKit
+
+final class TodayAuthorImageCollectionViewCell: BaseCollectionViewCell {
+    
+    private let authorImage: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFill
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 8
+        view.backgroundColor = .blue
+        return view
+    }()
+    
+    override func configureHierarchy() {
+        contentView.addSubview(authorImage)
+    }
+    
+    override func configureLayout() {
+        authorImage.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
