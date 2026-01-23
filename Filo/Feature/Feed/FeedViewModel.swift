97fa0052742f4c59b14e5ad52eb588f721fe5b3b
rmfls0606 <rmfls0606@naver.com>
2026-01-24 04:25:48 +0900

    feat: 피드 화면 접근 시 및 정렬 선택 시 필터 목록 조회 통신 스트림 로직 작성

--- Filo/Feature/Feed/FeedViewModel.swift
+++ Filo/Feature/Feed/FeedViewModel.swift
@@ -9,8 +9,6 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-
-
 final class FeedViewModel: ViewModelType {
     let category: String
     private let disposeBag = DisposeBag()
@@ -25,17 +23,40 @@ final class FeedViewModel: ViewModelType {
     
     struct Output{
         let selectedOrder: Driver<OrderByItem>
+        let filtersData: Driver<FilterSummaryPaginationListResponseEntity>
     }
     
     func transform(input: Input) -> Output {
         let selectedOrderRelay = BehaviorRelay<OrderByItem>(value: .popularity)
+        let filtersDataRelay = PublishRelay<FilterSummaryPaginationListResponseEntity>()
+        let networkErrorRelay = PublishRelay<NetworkError>()
+        let categoryRelay = BehaviorRelay<String>(value: category)
         
         input.orderByItemSelected
             .bind(to: selectedOrderRelay)
             .disposed(by: disposeBag)
 
+        Observable
+            .combineLatest(categoryRelay, selectedOrderRelay){ category, selected in
+                return (category: category, selected: selected)
+            }
+            .distinctUntilChanged{ $0.selected == $1.selected}
+            .subscribe(onNext: { parm in
+                Task{
+                    do{
+                        let dto: FilterSummaryPaginationListResponseDTO = try await NetworkManager.shared.request(FilterRouter.filters(next: "", limit: "", category: parm.category, orderBy: parm.selected.orderByName))
+                        filtersDataRelay.accept(dto.toEntity())
+                    }catch(let error as NetworkError){
+                        print(error)
+                        networkErrorRelay.accept(error)
+                    }
+                }
+            })
+            .disposed(by: disposeBag)
+
         return Output(
-            selectedOrder: selectedOrderRelay.asDriver()
+            selectedOrder: selectedOrderRelay.asDriver(),
+            filtersData: filtersDataRelay.asDriver(onErrorDriveWith: .empty())
         )
     }
 }
