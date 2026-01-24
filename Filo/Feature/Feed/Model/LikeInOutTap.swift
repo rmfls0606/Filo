b8a9b43c4cc9506bfcf26d1f0c3a950801d3bbc0
rmfls0606 <rmfls0606@naver.com>
2026-01-25 02:26:30 +0900

    feat: 필터 좋아요 상태 전달 타입 추가

---
+++ Filo/Feature/Feed/Model/LikeInOutTap.swift
@@ -0,0 +1,19 @@
+//
+//  LikeInOutTap.swift
+//  Filo
+//
+//  Created by 이상민 on 1/25/26.
+//
+
+import Foundation
+
+struct LikeInputTap {
+    let index: Int
+    let item: FilterSummaryResponseEntity
+}
+
+struct OutputLikeUpdate {
+    let filterId: String
+    let index: Int
+    let liked: Bool
+}
