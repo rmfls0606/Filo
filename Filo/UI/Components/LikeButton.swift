c45c84da99fe14e92c4ff2f85ebe85d93784b22a
rmfls0606 <rmfls0606@naver.com>
2026-01-25 03:53:17 +0900

    feat: 좋아요 버튼 컴포넌트 생성

---
+++ Filo/UI/Components/LikeButton.swift
@@ -0,0 +1,43 @@
+//
+//  LikeButton.swift
+//  Filo
+//
+//  Created by 이상민 on 1/25/26.
+//
+
+import UIKit
+
+final class LikeButton: UIButton {
+    var config = UIButton.Configuration.plain()
+    var imageConfig = UIImage.SymbolConfiguration(pointSize: 16, weight: .semibold)
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure(){
+        config.contentInsets = .zero
+        config.preferredSymbolConfigurationForImage = imageConfig
+        config.baseForegroundColor = .clear
+        config.baseBackgroundColor = .clear
+        config.image = UIImage(named: "like_Empty")
+        configuration = config
+        configurationUpdateHandler = { btn in
+            var config = btn.configuration
+            if btn.isSelected{
+                config?.image = UIImage(named: "like_Fill")
+                config?.baseForegroundColor = GrayStyle.gray30.color
+            }else{
+                config?.image = UIImage(named: "like_Empty")
+                config?.baseForegroundColor = GrayStyle.gray60.color
+            }
+            btn.configuration = config
+        }
+    }
+}
