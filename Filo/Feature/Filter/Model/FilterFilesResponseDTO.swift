8ba88fe1e920e4ff4dbf2068f274aa5edf44ceaf
rmfls0606 <rmfls0606@naver.com>
2026-01-25 21:37:29 +0900

    feat: 파일 얼로드 통신 응담 모델 DTO 정의

---
+++ Filo/Feature/Filter/Model/FilterFilesResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  FilterFilesResponseDTO.swift
+//  Filo
+//
+//  Created by 이상민 on 1/25/26.
+//
+
+import Foundation
+
+struct FilterFilesResponseDTO: Decodable {
+    let files: [String]
+}
