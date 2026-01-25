65281ed0b94a68723084b1193f9fbdad3ab53451
rmfls0606 <rmfls0606@naver.com>
2026-01-25 21:09:41 +0900

    feat: MultipartFile로 서버에 요청하기 위해 upload 사용함수 작성

--- Filo/Core/Network/NetworkManager.swift
+++ Filo/Core/Network/NetworkManager.swift
@@ -10,6 +10,7 @@ import Alamofire
 
 protocol NetworkManagerProtocol{
     func request<T: Decodable>(_ router: APITarget) async throws -> T
+    func upload<T: Decodable>(_ router: APITarget, files: [MultipartFile]) async throws -> T
 }
 
 final class NetworkManager: NetworkManagerProtocol{
@@ -19,10 +20,36 @@ final class NetworkManager: NetworkManagerProtocol{
     
     func request<T: Decodable>(_ router: APITarget) async throws -> T{
         let response = await AF.request(router.endPoint,
-                                          method: router.method,
-                                          parameters: router.parameters,
-                                          encoding: router.encoding,
-                                          headers: router.headers)
+                                        method: router.method,
+                                        parameters: router.parameters,
+                                        encoding: router.encoding,
+                                        headers: router.headers)
+            .validate(statusCode: 200..<300)
+            .serializingDecodable(T.self)
+            .response
+        
+        switch response.result{
+        case .success(let value):
+            return value
+        case .failure(let error):
+            throw NetworkError.mapping(error: error, statusCode: response.response?.statusCode, data: response.data)
+        }
+    }
+    
+    func upload<T: Decodable>(_ router: APITarget, files: [MultipartFile]) async throws -> T {
+        let response = await AF.upload(
+            multipartFormData: { form in
+                files.forEach { file in
+                    form.append(file.data,
+                                withName: file.name,
+                                fileName: file.fileName,
+                                mimeType: file.mimeType)
+                }
+            },
+            to: router.endPoint,
+            method: router.method,
+            headers: router.headers
+        )
             .validate(statusCode: 200..<300)
             .serializingDecodable(T.self)
             .response
@@ -35,3 +62,5 @@ final class NetworkManager: NetworkManagerProtocol{
         }
     }
 }
+
+
