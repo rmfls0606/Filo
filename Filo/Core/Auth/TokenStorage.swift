0cb286a6dfc3997c11f036149af06df584ae5302
rmfls0606 <rmfls0606@naver.com>
2026-01-29 23:12:23 +0900

    feat: 토큰 관리를 위한 토크 스토리지 생성

---
+++ Filo/Core/Auth/TokenStorage.swift
@@ -0,0 +1,52 @@
+//
+//  TokenStorage.swift
+//  Filo
+//
+//  Created by 이상민 on 1/21/26.
+//
+
+import Foundation
+
+//토큰 저장 + refresh 동시성 제어만
+final actor TokenStorage{
+    static let shared = TokenStorage()
+
+    private var refreshTask: Task<String, Error>?
+    
+    private init(){ }
+    
+    //MARK: - Read
+    func accessToken() async -> String?{
+        try? await KeychainManager.shared.read(key: .accessToken)
+    }
+    
+    func refreshToken() async -> String?{
+        try? await KeychainManager.shared.read(key: .refreshToken)
+    }
+    
+    //MARK: - Save
+    func save(access: String, refresh: String) async throws{
+        try await KeychainManager.shared.save(access, key: .accessToken)
+        try await KeychainManager.shared.save(refresh, key: .refreshToken)
+    }
+    
+    //MARK: - Clear
+    func clear() async{
+        await KeychainManager.shared.clearAll()
+    }
+    
+    //MARK: - Refresh 단일화
+    func refreshUpdate(_ refreshBlock: @escaping () async throws -> String) async throws -> String{
+        if let task = refreshTask{
+            return try await task.value
+        }
+        
+        let task = Task{
+            defer{ refreshTask = nil }
+            return try await refreshBlock()
+        }
+        
+        refreshTask = task
+        return try await task.value
+    }
+}
