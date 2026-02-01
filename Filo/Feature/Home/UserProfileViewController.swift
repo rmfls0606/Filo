989f6eb29f06811f1213dcf7bb1024d391377fe0
rmfls0606 <rmfls0606@naver.com>
2026-02-02 00:18:36 +0900

    feat: 다름 사람 프로필 화면 뷰 파일 생성

---
+++ Filo/Feature/Home/UserProfileViewController.swift
@@ -0,0 +1,31 @@
+//
+//  UserProfileViewController.swift
+//  Filo
+//
+//  Created by 이상민 on 2/2/26.
+//
+
+import UIKit
+import SnapKit
+
+final class UserProfileViewController: BaseViewController {
+    //MARK: - Property
+    
+    //MARK: - UI
+    
+    override func configureHierarchy() {
+        
+    }
+    
+    override func configureLayout() {
+        
+    }
+    
+    override func configureView() {
+        
+    }
+    
+    override func configureBind() {
+        
+    }
+}
