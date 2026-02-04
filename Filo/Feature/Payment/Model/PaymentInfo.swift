2ef807d51aab1046235bbd4688a7d6078329de12
rmfls0606 <rmfls0606@naver.com>
2026-02-04 22:44:48 +0900

    feat: 아임포트 SDK에 필요한 데이터 모델 생성

---
+++ Filo/Feature/Payment/Model/PaymentInfo.swift
@@ -0,0 +1,15 @@
+//
+//  PaymentInfo.swift
+//  Filo
+//
+//  Created by 이상민 on 2/4/26.
+//
+
+import Foundation
+
+struct PaymentInfo{
+    let merchantUId: String
+    let totalPrice: String
+    let productName: String
+    let buyerName: String
+}
