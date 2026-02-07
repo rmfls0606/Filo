ebf1a2e131884bd442cc3dbbccbab886fcabdd6c
rmfls0606 <rmfls0606@naver.com>
2026-02-07 18:01:31 +0900

    feat: 커뮤니티 디테일 전용 파일 생성

---
+++ Filo/Feature/Search/CommunityDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CommunityDetailViewController.swift
+//  Filo
+//
+//  Created by 이상민 on 2/7/26.
+//
+
+import UIKit
+
+final class CommunityDetailViewController: BaseViewController {
+    override func configureHierarchy() {
+        
+    }
+    
+    override func configureLayout() {
+        
+    }
+    
+    override func configureView() {
+        
+    }
+    
+    override func configureBind() {
+        
+    }
+}
---
+++ Filo/Feature/Search/CommunityDetailViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  CommunityDetailViewModel.swift
+//  Filo
+//
+//  Created by 이상민 on 2/7/26.
+//
+
+import UIKit
+
+final class CommunityDetailViewModel: ViewModelType {
+    struct Input{
+        
+    }
+    
+    struct Output{
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+}
